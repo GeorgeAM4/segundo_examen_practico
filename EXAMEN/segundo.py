479a6ef9608cd4fb33f4e1c02bde289915c8f795
GeorgeAM4 <www.apazajorgem1@gmail.com>
2026-09-16 08:37:39 -0400

    Update segundo.py

--- EXAMEN/segundo.py
+++ EXAMEN/segundo.py
@@ -6,12 +6,12 @@ tarifas = {
 while True:
     try:
         print("vehiculos")
-        print("1 Auto (10 Bs/hora)")
-        print("2 Moto (5 Bs/hora)")
-        print("3 Camión (20 Bs/hora)")
+        print("1 auto (10 Bs/hora)")
+        print("2 moto (5 Bs/hora)")
+        print("3 camion (20 Bs/hora)")
         opcion = input("seleccione el tipo de vehiculo (1, 2 o 3): ").strip()
         if opcion not in tarifas:
-            raise ValueError("opcion de vehiculo no válida.")
+            raise ValueError("opcion de vehiculo no valida.")
         horas = float(input("ingrese las horas de parqueo: "))
         if horas <= 0:
             raise ValueError("las horas deben ser mayores a 0.")
@@ -31,7 +31,7 @@ print("detalles de cobro2")
 print("="*35)
 print(f"tipo de vehículo: {tipo_vehiculo}")
 print(f"horas estacionado: {horas:.2f} hrs")
-print(f"tarifa por hora : {tarifa_hora} Bs")
+print(f"tarifa por hora : {tarifa_hora} Bss")
 print(f"subtotal        : {subtotal:.2f} Bs")
 if recargo > 0:
     print(f"recargo (15%)   : {recargo:.2f} Bs")
