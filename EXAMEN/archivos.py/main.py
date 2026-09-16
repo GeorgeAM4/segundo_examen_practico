b16e094f103fb2836774842ea12f091b9c55de68
GeorgeAM4 <www.apazajorgem1@gmail.com>
2026-09-16 08:53:44 -0400

    Update main.py

--- EXAMEN/archivos.py/main.py
+++ EXAMEN/archivos.py/main.py
@@ -1,6 +1,16 @@
-from validador import verofocador_de_acceso
+
+from validador import verificar_de_acceso
 def ejecutar_autenticacion():
-  try:
-    usuario_ingresado = input("ingrese su nombre de usuariu:"
-    clave_ingresada = input("ingrese su contraseña:"
-    if verificador_de_acceso(usuario_ingresado, clave
+    try:
+        usuario_ingresado = input("ingrese su nombre de usuario: ")
+        clave_ingresada = input("ingrese su contraseña: ")
+        if verificar_acceso(usuario_ingresado, clave_ingresada):
+            print("aceso concedido")
+        else:
+            print("credenciales incorrectas")
+    except KeyboardInterrupt:
+        print("\nEjecución cancelada por el usuario.")
+    except Exception as e:
+        print(f"Ocurrió un error inesperado durante el flujo: {e}")
+if __name__ == "__main__":
+    ejecutar_autenticacion()
