e06b0f657a42c67628e5f1611e855cf4c7ae09cf
GeorgeAM4 <www.apazajorgem1@gmail.com>
2026-09-16 08:45:45 -0400

    Update main.py

--- EXAMEN/archivos.py/main.py
+++ EXAMEN/archivos.py/main.py
@@ -1,1 +1,6 @@
-
+from validador import verofocador_de_acceso
+def ejecutar_autenticacion():
+  try:
+    usuario_ingresado = input("ingrese su nombre de usuariu:"
+    clave_ingresada = input("ingrese su contraseña:"
+    if verificador_de_acceso(usuario_ingresado, clave
